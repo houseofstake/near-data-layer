13c51c26d3791b06c92caca67e0f1009372de181
Pierre Haroun <pierre-210@hotmail.com>
2025-05-01 12:02:13 -0400

    adds indexing for chunks

--- schema.sql
+++ schema.sql
@@ -15,3 +15,24 @@ CREATE TABLE IF NOT EXISTS block_meta (
     gas_price TEXT NOT NULL,
     total_supply TEXT NOT NULL
 ); 
+
+CREATE TABLE IF NOT EXISTS chunk_meta (
+    height BIGINT NOT NULL,
+    chunk_hash TEXT NOT NULL,
+    prev_block_hash TEXT NOT NULL,
+    outcome_root TEXT NOT NULL,
+    prev_state_root TEXT NOT NULL,
+    encoded_merkle_root TEXT NOT NULL,
+    encoded_length BIGINT NOT NULL,
+    height_created BIGINT NOT NULL,
+    height_included BIGINT NOT NULL,
+    shard_id BIGINT NOT NULL,
+    gas_used BIGINT NOT NULL,
+    gas_limit BIGINT NOT NULL,
+    validator_reward TEXT NOT NULL,
+    balance_burnt TEXT NOT NULL,
+    outgoing_receipts_root TEXT NOT NULL,
+    tx_root TEXT NOT NULL,
+    author TEXT NOT NULL,
+    PRIMARY KEY (chunk_hash)
+); 
--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,22 @@ mod pb;
 use substreams::store::{self, DeltaProto, StoreSetIfNotExistsProto, StoreNew, StoreSetIfNotExists};
 use substreams_database_change::pb::database::{table_change::Operation, DatabaseChanges};
 use pb::sf::near::r#type::v1::Block;
-use pb::near::block_meta::v1::BlockMeta;
+use pb::near::block_meta::v1::{BlockMeta, ChunkMeta};
 use substreams::pb::substreams::store_delta::Operation as DeltaOperation;
 use chrono::{DateTime, Utc};
+
+/// move to utils
+fn bytes_to_string(bytes: &[u8]) -> String {
+    if !bytes.is_empty() {
+        let mut value = 0u128;
+        for &byte in bytes {
+            value = (value << 8) | (byte as u128);
+        }
+        value.to_string()
+    } else {
+        "0".to_string()
+    }
+}
 
 /// Process NEAR blocks and output database changes
 #[substreams::handlers::store]
@@ -24,48 +37,53 @@ fn store_block_meta(block: Block, s: StoreSetIfNotExistsProto<BlockMeta>) {
             prev_hash: if let Some(h) = &header.prev_hash { hex::encode(&h.bytes) } else { "".to_string() },
             author: block.author.clone(),
             timestamp,
-            gas_price: if let Some(gp) = &header.gas_price { 
-                // Access the bytes and convert to a number string
-                let bytes = &gp.bytes;
-                if !bytes.is_empty() {
-                    // Convert bytes to a number
-                    let mut value = 0u128;
-                    for &byte in bytes {
-                        value = (value << 8) | (byte as u128);
-                    }
-                    value.to_string()
-                } else {
-                    "0".to_string()
-                }
-            } else { 
-                "0".to_string() 
-            },
-            total_supply: if let Some(ts) = &header.total_supply {
-                // Access the bytes and convert to a number string
-                let bytes = &ts.bytes;
-                if !bytes.is_empty() {
-                    // Convert bytes to a number
-                    let mut value = 0u128;
-                    for &byte in bytes {
-                        value = (value << 8) | (byte as u128);
-                    }
-                    value.to_string()
-                } else {
-                    "0".to_string()
-                }
-            } else { 
-                "0".to_string() 
-            },
+            gas_price: if let Some(gp) = &header.gas_price { bytes_to_string(&gp.bytes) } else { "0".to_string() },
+            total_supply: if let Some(ts) = &header.total_supply { bytes_to_string(&ts.bytes) } else { "0".to_string() },
         };
         
         s.set_if_not_exists(header.height, header.height.to_string(), &block_meta);
     }
 }
 
+#[substreams::handlers::store]
+fn store_chunk_meta(block: Block, s: StoreSetIfNotExistsProto<ChunkMeta>) {
+    if let Some(header) = block.header.as_ref() {
+        for chunk_header in &block.chunk_headers {
+            let chunk_meta = ChunkMeta {
+                height: header.height,
+                chunk_hash: hex::encode(&chunk_header.chunk_hash),
+                prev_block_hash: hex::encode(&chunk_header.prev_block_hash),
+                outcome_root: hex::encode(&chunk_header.outcome_root),
+                prev_state_root: hex::encode(&chunk_header.prev_state_root),
+                encoded_merkle_root: hex::encode(&chunk_header.encoded_merkle_root),
+                encoded_length: chunk_header.encoded_length,
+                height_created: chunk_header.height_created,
+                height_included: chunk_header.height_included,
+                shard_id: chunk_header.shard_id,
+                gas_used: chunk_header.gas_used,
+                gas_limit: chunk_header.gas_limit,
+                validator_reward: if let Some(vr) = &chunk_header.validator_reward { bytes_to_string(&vr.bytes) } else { "0".to_string() },
+                balance_burnt: if let Some(bb) = &chunk_header.balance_burnt { bytes_to_string(&bb.bytes) } else { "0".to_string() },
+                outgoing_receipts_root: hex::encode(&chunk_header.outgoing_receipts_root),
+                tx_root: hex::encode(&chunk_header.tx_root),
+                author: block.author.clone(),
+            };
+
+            let key = format!("{}-{}", header.height, hex::encode(&chunk_header.chunk_hash));
+            s.set_if_not_exists(header.height, key, &chunk_meta);
+        }
+    }
+}
+
 #[substreams::handlers::map]
-fn db_out(block_meta_deltas: store::Deltas<DeltaProto<BlockMeta>>) -> Result<DatabaseChanges, substreams::errors::Error> {
+fn db_out(block_meta_deltas: store::Deltas<DeltaProto<BlockMeta>>,
+          chunk_meta_deltas: store::Deltas<DeltaProto<ChunkMeta>>) 
+    -> Result<DatabaseChanges, substreams::errors::Error> {
     let mut database_changes = DatabaseChanges::default();
+    
     transform_block_meta_to_database_changes(&mut database_changes, block_meta_deltas);
+    transform_chunk_meta_to_database_changes(&mut database_changes, chunk_meta_deltas);
+    
     Ok(database_changes)
 }
 
@@ -75,13 +93,25 @@ fn transform_block_meta_to_database_changes(
 ) {
     for delta in deltas.deltas {
         match delta.operation {
-            DeltaOperation::Create => push_create(changes, &delta.key, delta.ordinal, &delta.new_value),
+            DeltaOperation::Create => push_create_block_meta(changes, &delta.key, delta.ordinal, &delta.new_value),
             _ => {}
         }
     }
 }
 
-fn push_create(
+fn transform_chunk_meta_to_database_changes(
+    changes: &mut DatabaseChanges,
+    deltas: store::Deltas<DeltaProto<ChunkMeta>>,
+) {
+    for delta in deltas.deltas {
+        match delta.operation {
+            DeltaOperation::Create => push_create_chunk_meta(changes, &delta.key, delta.ordinal, &delta.new_value),
+            _ => {}
+        }
+    }
+}
+
+fn push_create_block_meta(
     changes: &mut DatabaseChanges,
     key: &str,
     ordinal: u64,
@@ -96,4 +126,31 @@ fn push_create(
         .change("timestamp", (None, &value.timestamp))
         .change("gas_price", (None, &value.gas_price))
         .change("total_supply", (None, &value.total_supply));
+}
+
+fn push_create_chunk_meta(
+    changes: &mut DatabaseChanges,
+    key: &str,
+    ordinal: u64,
+    value: &ChunkMeta,
+) {
+    changes
+        .push_change("chunk_meta", key, ordinal, Operation::Create)
+        .change("height", (None, value.height))
+        .change("chunk_hash", (None, &value.chunk_hash))
+        .change("prev_block_hash", (None, &value.prev_block_hash))
+        .change("outcome_root", (None, &value.outcome_root))
+        .change("prev_state_root", (None, &value.prev_state_root))
+        .change("encoded_merkle_root", (None, &value.encoded_merkle_root))
+        .change("encoded_length", (None, value.encoded_length))
+        .change("height_created", (None, value.height_created))
+        .change("height_included", (None, value.height_included))
+        .change("shard_id", (None, value.shard_id))
+        .change("gas_used", (None, value.gas_used))
+        .change("gas_limit", (None, value.gas_limit))
+        .change("validator_reward", (None, &value.validator_reward))
+        .change("balance_burnt", (None, &value.balance_burnt))
+        .change("outgoing_receipts_root", (None, &value.outgoing_receipts_root))
+        .change("tx_root", (None, &value.tx_root))
+        .change("author", (None, &value.author));
 }
