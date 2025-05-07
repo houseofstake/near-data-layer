455b0756cf3e738bb8fd94f91f9f33d080fbe507
Pierre Haroun <pierre-210@hotmail.com>
2025-05-06 22:00:34 -0400

    adds start script to fetch secrets from gcp secrets manager on docker run

---
+++ scripts/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set environment (default to dev if not specified)
+ENV=${ENVIRONMENT:-dev}
+
+# Fetch secrets from Secret Manager
+export DATABASE_USER=$(gcloud secrets versions access latest --secret=DATABASE_USER)
+export DATABASE_PASSWORD=$(gcloud secrets versions access latest --secret=DATABASE_PASSWORD)
+export SUBSTREAMS_API_KEY=$(gcloud secrets versions access latest --secret=PINAX_API_KEY)
+export ENDPOINT=$(gcloud secrets versions access latest --secret=PINAX_ENDPOINT_${ENV^^})
+
+# Construct DSN
+export DSN="postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@10.20.179.3:5432/postgres?sslmode=disable"
+
+# Run the sink
+make setup_sink run_sink
