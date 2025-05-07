1decf363120d6b62c9c2944bcfc159f71118ab07
Pierre Haroun <pierre-210@hotmail.com>
2025-05-07 13:43:59 -0400

    paramaterize DB HOST

--- scripts/fetch_secrets.sh
+++ scripts/fetch_secrets.sh
@@ -6,8 +6,9 @@ ENV=${ENVIRONMENT:-DEV}
 # Fetch secrets from Secret Manager
 export DATABASE_USER=$(gcloud secrets versions access latest --secret=DATABASE_USER)
 export DATABASE_PASSWORD=$(gcloud secrets versions access latest --secret=DATABASE_PASSWORD)
+export DATABASE_HOST=$(gcloud secrets versions access latest --secret=DATABASE_HOST_${ENV})
 export SUBSTREAMS_API_KEY=$(gcloud secrets versions access latest --secret=PINAX_API_KEY)
 export ENDPOINT=$(gcloud secrets versions access latest --secret=PINAX_ENDPOINT_${ENV})
 
 # Construct DSN
-export DSN="postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@10.20.179.3:5432/postgres?sslmode=disable"
+export DSN="postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:5432/postgres?sslmode=disable"
