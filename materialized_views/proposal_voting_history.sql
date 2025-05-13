cf0754a530270098dd1488d06d08c5f73d46684a
elai-boosted <elaine@getboosted.io>
2025-05-13 09:51:47 -0400

    added proposal_voting_history file (#18)

---
+++ materialized_views/proposal_voting_history.sql
@@ -0,0 +1,105 @@
+/*
+ Primary key on this table is receipt_id, with base58 encoding. 
+ Every single row in this table is a unique, successful vote action.
+ ("Successful" is defined by pulling only receipt_ids flagged as successful from the execution_outcomes table.)
+ 
+ Each vote action is associated with: 
+   1. A voter account 
+   2. The proposal voted on 
+   3. The vote option made 
+   4. The voting power of the voter at the time the vote was executed (from execution_outcomes.logs)
+   5. The voting power delegated on a vote action 
+   6. The timestamp at which the vote action occurred 
+   7. The block-related data for this vote (block hash/id, chunk hash/id, block height) 
+*/
+
+CREATE MATERIALIZED VIEW proposal_voting_history AS
+WITH execution_outcomes_prep AS (
+	SELECT 
+		SPLIT_PART(receipt_id, '-', 2) AS receipt_id 
+		, status
+		, logs
+	FROM execution_outcomes 
+)
+, receipt_actions_prep AS (
+  	SELECT 
+    	decode(ra.args_base64, 'base64') AS args
+    	, ra.*
+    	, eo.logs 
+  	FROM receipt_actions AS ra
+  	INNER JOIN execution_outcomes_prep AS eo 
+ 		ON ra.receipt_id = eo.receipt_id 
+ 		AND eo.status = 'SuccessValue'
+  	WHERE 
+    	ra.action_kind = 'FunctionCall'
+    	AND ra.method_name = 'vote'
+    	AND ra.receiver_id IN (           --House of Stake contracts
+    		'v.r-1745564650.testnet'      --veNEAR contract 
+    		, 'vote.r-1745564650.testnet' --Voting contract
+    		) 
+)
+, proposal_voting_history AS (
+	SELECT 
+		base58_encode(ra.receipt_id)   AS id  
+		, base58_encode(ra.receipt_id) AS receipt_id 
+		, DATE(ra.block_timestamp)     AS voted_date 
+		, ra.block_timestamp           AS voted_at 
+	
+		--IDs 																					
+		, (convert_from(ra.args, 'UTF8')::json->>'proposal_id')::numeric AS proposal_id
+		, ra.receiver_id    											 AS hos_contract_address 
+		, ra.predecessor_id 											 AS voter_id 
+	
+		/* Voter Data Per Proposal */
+		--Votes Info
+		, (convert_from(ra.args, 'UTF8')::json->>'vote')::numeric                                				AS vote_option
+		, (REPLACE(ra.logs[1], 'EVENT_JSON:', '')::json->'data'->0->>'account_balance')::numeric 			    AS voting_power
+		, (convert_from(ra.args, 'UTF8')::json->'v_account'->'V0'->'balance'->>'near_balance')::numeric         AS near_balance 
+		, (convert_from(ra.args, 'UTF8')::json->'v_account'->'V0'->'balance'->>'extra_venear_balance')::numeric AS extra_venear_balance
+	
+    	--Delegation Info
+		, (convert_from(ra.args, 'UTF8')::json->'v_account'->'V0'->'delegation'->>'account_id')        					  AS delegator_account_id 
+		, (convert_from(ra.args, 'UTF8')::json->'v_account'->'V0'->'delegated_balance'->>'near_balance')::numeric         AS delegated_near_balance
+		, (convert_from(ra.args, 'UTF8')::json->'v_account'->'V0'->'delegated_balance'->>'extra_venear_balance')::numeric AS delegated_extra_venear_balance
+	
+		--Logs 
+		, ra.logs
+	
+		--Block Data 
+		, ra.block_height 
+		, base58_encode(ra.block_hash) AS block_hash 
+		, base58_encode(ra.chunk_hash) AS chunk_hash
+	FROM receipt_actions_prep AS ra
+ 	WHERE 
+		(convert_from(ra.args, 'UTF8')::json->>'proposal_id')::numeric IS NOT NULL
+	ORDER BY proposal_id ASC, voted_at ASC 
+)
+, latest_vote_per_proposal_and_voter AS (
+	SELECT 
+		*
+		, ROW_NUMBER() OVER (PARTITION BY proposal_id, voter_id ORDER BY voted_at DESC) as row_num 
+	FROM proposal_voting_history 
+)
+SELECT 
+	id 
+	, receipt_id 
+	, voted_date 
+	, voted_at 
+	, proposal_id 
+	, hos_contract_address 
+	, voter_id 
+	, vote_option 
+	, voting_power 
+	, near_balance 
+	, extra_venear_balance 
+	, delegator_account_id 
+	, delegated_near_balance 
+	, delegated_extra_venear_balance 
+	, block_height 
+	, block_hash
+	, chunk_hash 
+FROM latest_vote_per_proposal_and_voter
+WHERE 
+	row_num = 1
+WITH DATA
+;
