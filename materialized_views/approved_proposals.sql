f4ae831553df5ea55943e36d02c7de01bbd09477
elai-boosted <elaine@getboosted.io>
2025-05-14 16:59:30 -0400

    Added two new views, registered_voters and approved_proposals

---
+++ materialized_views/approved_proposals.sql
@@ -0,0 +1,60 @@
+/*
+ Primary key on this table is receipt_id, with base58 encoding. 
+ Every single row in this table is a unique, successful approve_proposal action.
+ ("Successful" is defined by pulling only receipt_ids flagged as successful from the execution_outcomes table.)
+ 
+ Each approve_proposal action is associated with: 
+   1. A proposal approver ID                                              (eg. lighttea2007.testnet) 
+   2. The related House of Stake Contract                                 (Voter contract address, vote.r-1745564650.testnet)
+   3. The date + timestamp at which the proposal approval action occurred 
+   4. The block-related data for this approve_proposal action             (Block hash/id, chunk hash/id, block height) 
+*/
+
+
+CREATE MATERIALIZED VIEW approved_proposals AS 
+WITH execution_outcomes_prep AS (
+	SELECT 
+		SPLIT_PART(receipt_id, '-', 2) AS receipt_id 
+		, status
+		, logs
+	FROM execution_outcomes 
+)
+, approve_proposal_action_prep AS ( 
+ 	SELECT 
+    	decode(ra.args_base64, 'base64') AS args
+    	, eo.status 
+    	, eo.logs
+    	, base58_encode(ra.receipt_id) AS encoded_receipt_id
+    	, ra.*
+  	FROM receipt_actions AS ra
+  	INNER JOIN execution_outcomes_prep AS eo 
+ 		ON ra.receipt_id = eo.receipt_id 
+	 	AND eo.status = 'SuccessReceiptId'
+  	WHERE 
+    	ra.action_kind = 'FunctionCall'
+    	AND ra.method_name = 'approve_proposal'
+    	AND ra.receiver_id IN (
+    		'v.r-1745564650.testnet'      --veNEAR contract 
+    		, 'vote.r-1745564650.testnet' --Voting contract
+    		) 
+  	ORDER BY block_height DESC
+ )
+ SELECT
+ 	base58_encode(ra.receipt_id) AS id 
+ 	, base58_encode(ra.receipt_id) AS receipt_id 
+ 	, DATE(ra.block_timestamp)     AS proposal_approved_date
+ 	, ra.block_timestamp           AS proposal_approved_at
+ 	
+ 	--Proposal Details 
+ 	, ra.receiver_id           												   AS hos_contract_address
+ 	, (convert_from(ra.args, 'UTF8')::json->>'proposal_id')::numeric           AS proposal_id     
+ 	, ra.signer_account_id     												   AS proposal_approver_id 
+
+ 	--Block details 
+ 	, ra.block_hash 
+ 	, ra.block_height
+ 	, ra.chunk_hash 
+ FROM approve_proposal_action_prep AS ra
+ ORDER BY block_timestamp DESC
+ WITH DATA	
+ ; 
