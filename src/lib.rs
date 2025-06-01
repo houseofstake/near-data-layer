f84d053b977c700a3f34a6c439cb72b14534245b
Shady Ibrahim <ImShady@users.noreply.github.com>
2025-06-01 14:25:40 -0400

    Revert "Reintroduce stores (#25)" (#26)
    This reverts commit 8bbf13507d0d68c4777cea2de9ab820cc93ab58c.

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,18 @@
 mod pb;
 mod processors;
 mod pushers;
-mod stores;
-mod transforms;
 pub mod config;
 
 use substreams_database_change::pb::database::DatabaseChanges;
-use substreams::store::{self, DeltaProto};
-
-// Store handlers and transform functions
-use crate::transforms::*;
+use crate::processors::process_block;
+use crate::pb::sf::near::r#type::v1::Block;
 
 #[substreams::handlers::map]
-fn db_out(
-    blocks_deltas: store::Deltas<DeltaProto<pb::near::entities::v1::Block>>,
-    receipts_deltas: store::Deltas<DeltaProto<pb::near::entities::v1::Receipt>>,
-    receipt_actions_deltas: store::Deltas<DeltaProto<pb::near::entities::v1::ReceiptAction>>,
-    execution_outcomes_deltas: store::Deltas<DeltaProto<pb::near::entities::v1::ExecutionOutcome>>,
-) -> Result<DatabaseChanges, substreams::errors::Error> {
+fn db_out(block: Block) -> Result<DatabaseChanges, substreams::errors::Error> {
     let mut database_changes: DatabaseChanges = Default::default();
     
-    // Transform all store deltas into database changes
-    transform_blocks_deltas(&mut database_changes, blocks_deltas);
-    transform_receipts_deltas(&mut database_changes, receipts_deltas);
-    transform_receipt_actions_deltas(&mut database_changes, receipt_actions_deltas);
-    transform_execution_outcomes_deltas(&mut database_changes, execution_outcomes_deltas);
+    // Process the block and all its nested data
+    process_block(&mut database_changes, &block);
 
     Ok(database_changes)
 }
