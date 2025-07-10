2a7964b207a9452e0713d3c4178df5b23bb68a57
elai-boosted <elaine@getboosted.io>
2025-07-10 12:03:14 -0400

    feat(axb-167): Updated proposals with 2 new cols, voting_duration_ns + total_venear_at_approval (#49)
    * Updated proposals with 2 new cols
    
    * spacing
    
    * added safe_json parsing to proposal view changes

--- sql_views/proposals.sql
+++ sql_views/proposals.sql
@@ -15,6 +15,7 @@ WITH execution_outcomes_prep AS (
  		SPLIT_PART(receipt_id, '-', 2) AS receipt_id
  		, status
  		, logs
+		, results_json
  	FROM execution_outcomes
 )
 , receipt_actions_prep AS (
@@ -22,6 +23,8 @@ WITH execution_outcomes_prep AS (
  		decode(ra.args_base64, 'base64') AS args_decoded
  		, eo.status AS action_status
  		, eo.logs AS action_logs
+		, eo.results_json
+		, base58_encode(ra.receipt_id) AS receipt_id_encoded
  		, ra.*
  	FROM receipt_actions AS ra
  	INNER JOIN execution_outcomes_prep AS eo
@@ -77,6 +80,12 @@ WITH execution_outcomes_prep AS (
  	SELECT
  		base58_encode(ra.receipt_id) AS id
  		, base58_encode(ra.receipt_id) AS receipt_id
+		--From associated on_get_snapshot method 
+		, CASE 
+ 			WHEN safe_json_parse(ra.results_json)->>'error' IS NULL
+ 			THEN base58_encode(safe_json_parse(ra.results_json)->>'receipt_id') 
+ 			ELSE NULL
+ 		  END AS snapshot_receipt_id 
  		, DATE(ra.block_timestamp) AS proposal_approved_date
  		, ra.block_timestamp AS proposal_approved_at
 
@@ -93,6 +102,25 @@ WITH execution_outcomes_prep AS (
  	WHERE
  		ra.method_name = 'approve_proposal'
  )
+ , approve_proposal_snapshot_metadata AS (
+ 	SELECT 
+ 		ap.proposal_id
+ 		, ap.receipt_id AS approve_proposal_receipt_id
+ 		, ra.receipt_id_encoded AS snapshot_receipt_id
+        , CASE 
+ 			WHEN safe_json_parse(ra.results_json)->>'error' IS NULL
+ 			THEN (safe_json_parse(ra.results_json)->'snapshot_and_state'->>'total_venear')::NUMERIC
+ 			ELSE NULL
+ 		  END AS total_venear_amount 
+ 	    , CASE 
+ 			WHEN safe_json_parse(ra.results_json)->>'error' IS NULL
+ 			THEN (safe_json_parse(ra.results_json)->>'voting_duration_ns')::NUMERIC
+ 			ELSE NULL
+ 		   END AS voting_duration_ns 
+ 	FROM receipt_actions_prep AS ra
+ 	INNER JOIN approve_proposal AS ap 
+ 		ON ra.receipt_id_encoded = ap.snapshot_receipt_id
+)
  , reject_proposal as (
  	SELECT
  		base58_encode(ra.receipt_id) AS id
@@ -164,6 +192,10 @@ WITH execution_outcomes_prep AS (
  	--Rejection Details 
  	, rp.proposal_rejected_at AS rejected_at 
  	, rp.proposal_rejecter_id AS rejecter_id 
+
+	--Additional Approval Metadata (Sourced from associated on_get_snapshot method)
+ 	, aps.voting_duration_ns 
+ 	, aps.total_venear_amount AS total_venear_at_approval
  	
  	--Vote Details 
  	, pv.listagg_distinct_voters
@@ -181,6 +213,8 @@ WITH execution_outcomes_prep AS (
  FROM create_proposal AS cp
  LEFT JOIN approve_proposal ap
  	ON cp.proposal_id = ap.proposal_id
+ LEFT JOIN approve_proposal_snapshot_metadata AS aps
+ 	ON cp.proposal_id = aps.proposal_id 
  LEFT JOIN reject_proposal AS rp 
  	ON cp.proposal_id = rp.proposal_id 
  LEFT JOIN proposal_votes AS pv 
